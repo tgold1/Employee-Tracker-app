ee074094cbc99d086e4bb53bb03b903675ec785c
Tim Gold <timothygold@comcast.net>
2023-04-23 17:51:48 -0400

    added function in index.js to view data for view all employees

--- index.js
+++ index.js
@@ -108,3 +108,12 @@ function viewAllRoles () {
        });
 }
 
+function viewAllemployees () {
+    const query = `SELECT e.id AS "Employee Id", e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS "Job Title", department.name AS Department, role.salary AS Salary, m.first_name AS "Manager First Name", m.last_name AS "Manager Last Name" FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY e.id`;
+    connection.query(query, (err, result) => {
+        if (err) {
+            console.log(err);
+          }
+          console.table(result);
+       });
+}
