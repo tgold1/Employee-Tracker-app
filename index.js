705b1d1e9600aca88c35a3fc25c9d4b0787d07f8
Tim Gold <timothygold@comcast.net>
2023-04-23 15:25:19 -0400

    added prompt messages and the first prompt questions in index.js

---
+++ index.js
@@ -0,0 +1,92 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const promptMessages = {
+    viewALLDepartments: "View all departments",
+    viewAllRoles: "View all roles",
+    viewAllemployees: "View all employees",
+    addaDepartment: "Add a department",
+    addaRole: "Add a role",
+    addanEmployee: "Add an employee",
+    updateanEmployeeRole: "Update an employee role",
+    quit: "Quit"
+};
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: 'root',
+
+    // Your password
+    password: 'Phillies',
+    database: 'employees'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    prompt();
+});
+
+function prompt() {
+    inquirer
+        .prompt({
+            name: 'options',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                promptMessages.viewALLDepartments,
+                promptMessages.viewAllRoles,
+                promptMessages.viewAllemployees,
+                promptMessages.addaDepartment,
+                promptMessages.addaRole,
+                promptMessages.addanEmployee,
+                promptMessages.updateanEmployeeRole,
+                promptMessages.quit
+            ]
+        })
+        .then(answer => {
+            console.log('answer', answer);
+            switch (answer.options) {
+                case promptMessages.viewALLDepartments:
+                    viewALLDepartments();
+                    break;
+                
+                case promptMessages.viewAllRoles:
+                    viewAllRoles();
+                    break;
+
+                case promptMessages.viewAllemployees:
+                    viewAllemployees();
+                    break;
+                
+                case promptMessages.addaDepartment:
+                    addaDepartment();
+                    break;
+
+                case promptMessages.addaRole:
+                    addaRole();
+                    break;
+                
+                case promptMessages.addanEmployee:
+                    addanEmployee();
+                    break;
+
+                case promptMessages.updateanEmployeeRole:
+                    updateanEmployeeRole();
+                    break;
+
+                case promptMessages.quit:
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function viewALLDepartments () {
+   const query = SELECT 
+}
